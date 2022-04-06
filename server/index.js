9bf0d733b009c12da86a1fe97e1111ec7b48e633
kayleen <8iscogb9@anonaddy.me>
2022-04-06 11:37:09 -0500

    added path

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const app = express()
 
+const path = require('path')
 // endpoint
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
